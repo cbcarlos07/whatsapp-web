49f9ef0ef430d7a1f5888ff0aafae2555c40301c
carlos <cbcarlos08@gmail.com>
2024-03-27 09:28:39 -0300

    Inicial

---
+++ src/main.js
@@ -0,0 +1,25 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+// Create a new client instance
+const client = new Client();
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Client is ready!');
+});
+
+// When the client received QR-Code
+client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('message_create', message => {
+	if (message.body === '!ping') {
+		// send back "pong" to the chat the message was sent in
+		client.sendMessage(message.from, 'pong');
+	}
+});
+
+// Start your client
+client.initialize();
